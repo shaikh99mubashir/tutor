49f80755f3dc523fabdeacd767cf385bb0ebb4cc
Shaikh Mubashir <39335986+shaikh99mubashir@users.noreply.github.com>
2023-07-08 13:11:53 +0500

    update

--- Screens/ReportSubmission/index.tsx
+++ Screens/ReportSubmission/index.tsx
@@ -7,39 +7,16 @@ import {
   Image,
   Modal,
   Button,
+  TextInput,
+  Dimensions,
 } from 'react-native';
 import React, {useState} from 'react';
 import {Theme} from '../../constant/theme';
 import Header from '../../Component/Header';
 import DropDownPicker from 'react-native-dropdown-picker';
+import DropDownModalView from '../../Component/DropDownModalView';
 
 const ReportSubmission = ({navigation}: any) => {
-  const [selectedServicedata, setSelectedServicedata]: any = useState({});
-  const [serviceDD, setServiceDD] = useState(false);
-  const [reportType, setReportType] = useState(null);
-  const [student, setStudent] = useState(null);
-  const [subject, setSubject] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
-  
-  const handleReportTypeChange = (value: any) => {
-     setServiceDD(!serviceDD);
-    setModalVisible(true);
-  };
-
-  const setReportTypeChange = (value: any) => {
-    setReportType(value);
-    setModalVisible(false);
-    setServiceDD(!serviceDD);
-  };
-  const handleStudentChange = (value: any) => {
-    setStudent(value);
-    setModalVisible(false);
-  };
-
-  const handleSubjectChange = (value: any) => {
-    setSubject(value);
-    setModalVisible(false);
-  };
   const EvalutionOption = [
     {
       option: 'Hello World',
@@ -52,62 +29,18 @@ const ReportSubmission = ({navigation}: any) => {
     <View style={{backgroundColor: Theme.white, height: '100%'}}>
       <Header title="Report Submission" backBtn navigation={navigation} />
       <ScrollView showsVerticalScrollIndicator={false} nestedScrollEnabled>
-        <View style={{paddingHorizontal: 15}}>
-            {/* Report Type */}
-          <View style={{marginTop: 8}}>
-            <Text style={{fontSize: 14, fontWeight: 'bold', color: 'black'}}>
-              Report Type
-            </Text>
-            <TouchableOpacity
-              activeOpacity={0.8}
-              onPress={handleReportTypeChange}
-              style={{
-                marginTop: 5,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                paddingVertical: 10,
-                paddingHorizontal: 15,
-                borderWidth: 1,
-                borderTopLeftRadius: 5,
-                borderTopRightRadius: 5,
-                borderBottomLeftRadius: 5,
-                borderBottomRightRadius: 5,
-                borderColor: Theme.gray,
-                alignItems: 'center',
-              }}>
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}>
-                  <Text
-                    style={{
-                      color: Theme.gray,
-                      fontFamily: 'Poppins-SemiBold',
-                      fontSize: 16,
-                    }}>
-                    {reportType ? reportType : 'Evaluation Report'}
-                  </Text>
-                  {serviceDD ? (
-                    <Image
-                      source={require('../../Assets/Images/up.png')}
-                      style={{width: 15, height: 20}}
-                      resizeMode="contain"
-                    />
-                  ) : (
-                    <Image
-                      source={require('../../Assets/Images/down.png')}
-                      style={{width: 20, height: 20}}
-                    />
-                  )}
-                </View>
-            </TouchableOpacity>
-          </View>
+        <View style={{paddingHorizontal: 15, marginBottom: 100}}>
+          {/* Report Type */}
+          <DropDownModalView
+            title="Report Type"
+            placeHolder="Evaluation Report"
+            option={EvalutionOption}
+            modalHeading="Select Report Type"
+          />
           {/* First Class Date */}
           <View style={{marginTop: 8}}>
             <Text style={{fontSize: 14, fontWeight: 'bold', color: 'black'}}>
-            First Class Date
+              First Class Date
             </Text>
             <View
               style={{
@@ -124,53 +57,134 @@ const ReportSubmission = ({navigation}: any) => {
                 borderColor: Theme.gray,
                 alignItems: 'center',
               }}>
-                
-                  <Text
-                    style={{
-                      color: Theme.gray,
-                      fontFamily: 'Poppins-SemiBold',
-                      fontSize: 16,
-                    }}>
-                    7 June 2023
-                  </Text>
-
+              <Text
+                style={{
+                  color: Theme.gray,
+                  fontFamily: 'Poppins-SemiBold',
+                  fontSize: 16,
+                }}>
+                7 June 2023
+              </Text>
             </View>
+          </View>
+          {/* Student */}
+          <DropDownModalView
+            title="Student"
+            placeHolder="Mk Test Student"
+            option={EvalutionOption}
+            modalHeading="Student"
+          />
+          {/* Subject */}
+          <DropDownModalView
+            title="Subject"
+            placeHolder="Select Subject"
+            option={EvalutionOption}
+            modalHeading="Subject"
+          />
+          {/* Knowledge */}
+          <DropDownModalView
+            title="1. Knowledge"
+            subTitle="What can you tell us about the student's knowledge of this Subject"
+            placeHolder="Select Answer"
+            option={EvalutionOption}
+            modalHeading="Knowledge"
+          />
+          {/* Understanding */}
+          <DropDownModalView
+            title="2. Understanding"
+            subTitle="What can you tell us about the student's Understanding of this Subject"
+            placeHolder="Select Answer"
+            option={EvalutionOption}
+            modalHeading="Understanding"
+          />
+          {/* Analysis */}
+          <DropDownModalView
+            title="3. Analysis"
+            subTitle="What can you tell us about the student's ablity to apply analyse fact and theory of this Subject"
+            placeHolder="Select Answer"
+            option={EvalutionOption}
+            modalHeading="Analysis"
+          />
+
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: 'bold',
+              color: 'black',
+              marginTop: 8,
+            }}>
+            4. Additional Assessment
+          </Text>
+          <Text
+            style={{
+              color: Theme.gray,
+              fontFamily: 'Poppins-SemiBold',
+              fontSize: 16,
+            }}>
+            What is the current score for the subject?
+          </Text>
+          <View
+            style={[
+              styles.textAreaContainer,
+              {
+                width: '100%',
+                borderWidth: 1,
+                borderRadius: 10,
+                marginTop: 5,
+              },
+            ]}>
+            <TextInput
+              placeholder="Message"
+              multiline={true}
+              maxLength={300}
+              style={[
+                styles.textArea,
+                {
+                  width: Dimensions.get('window').width,
+                  padding: 8,
+                },
+              ]}
+              underlineColorAndroid="transparent"
+              placeholderTextColor="grey"
+            />
+          </View>
+          <Text
+            style={{
+              color: Theme.gray,
+              fontFamily: 'Poppins-SemiBold',
+              fontSize: 16,
+              marginTop:8
+            }}>
+            Elaborate your Plan to Help Student?
+          </Text>
+          <View
+            style={[
+              styles.textAreaContainer,
+              {
+                width: '100%',
+                borderWidth: 1,
+                borderRadius: 10,
+                marginTop: 5,
+              },
+            ]}>
+            <TextInput
+              placeholder="Plan"
+              multiline={true}
+              maxLength={300}
+              style={[
+                styles.textArea,
+                {
+                  height: 80,
+                  padding: 8,
+                },
+              ]}
+              underlineColorAndroid="transparent"
+              placeholderTextColor="grey"
+            />
           </View>
         </View>
       </ScrollView>
-        <Modal visible={modalVisible} animationType="slide" transparent={true}>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'rgba(0,0,0,0.5)',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <View
-              style={{
-                backgroundColor: 'white',
-                padding: 20,
-                borderTopRightRadius:15,
-                borderTopLeftRadius:15,
-                position:'absolute',
-                bottom:0,
-                height:'40%',
-                width:'100%'
-              }}>
-              <TouchableOpacity onPress={() => setModalVisible(false)}>
-                <Text style={{textAlign:'right'}}>X</Text>
-              </TouchableOpacity>
-              <Text style={{fontSize: 14, fontWeight: 'bold', color: 'black',marginBottom:15}}>Select Report Type</Text>
-              {EvalutionOption && EvalutionOption.map((e:any,i)=>{
-                return(
-                    <TouchableOpacity key={i} onPress={()=> setReportTypeChange(e.option)} style={{marginVertical:5}}>
-                    <Text style={{fontSize: 14, fontWeight: 'bold', color: 'black'}}>{e.option}</Text>
-                    </TouchableOpacity>
-                )
-              })}
-            </View>
-          </View>
-        </Modal>
+
       {/* Submit Button */}
       <View
         style={{
@@ -213,4 +227,14 @@ const ReportSubmission = ({navigation}: any) => {
 
 export default ReportSubmission;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textAreaContainer: {
+    borderWidth: 1,
+    padding: 5,
+  },
+  textArea: {
+    height: 120,
+    justifyContent: 'flex-start',
+    textAlignVertical: 'top',
+  },
+});
